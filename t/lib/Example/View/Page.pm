7342ca22754975be5c33b13bcbe01cdbb8396ccb
John Napiorkowski <jjn1056@yahoo.com>
2024-10-20 20:39:16 -0500

    fix autocorrect issue

--- t/lib/Example/View/Page.pm
+++ t/lib/Example/View/Page.pm
@@ -1,40 +1,25 @@
-BEGIN {
-  use Test::Most;
-  eval "use Catalyst 5.90090; 1" || do {
-    plan skip_all => "Need a newer version of Catalyst => $@";
-  };
-}
+package Example::View::Page;
 
-use Test::Most;
-use Test::Lib;
-use HTTP::Request::Common;
-use Catalyst::Test 'Example';
+use Moose;
 
-{
-  ok my $res = request GET '/hello';
-  ok my $data = $res->content;
-  
-  is $data, '<!doctype html>
+extends 'Example::View::Base';
+
+has title => (is=>'ro', isa=>'Str', required=>1, default=>'Missing Title');
+
+__PACKAGE__->meta->make_immutable;
+__DATA__
+<!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello</title>
+    <title><%= $self->title %></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    
-    <h1>Hello</h1>
-    &lt;a href=&#39;/hello&#39;&gt;Hello&lt;/a&gt;
-<form accept-charset="UTF-8" enctype="application/x-www-form-urlencoded" method="post">
-<input id="person_name" name="person.name" type="text" value=""/>
-<input id="person_age" name="person.age" type="text" value=""/>
-</form>
+    <%= $content %>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
   </body>
-</html>';
-}
-
-done_testing;
+</html>
